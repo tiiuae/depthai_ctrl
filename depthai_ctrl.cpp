7be239b6038625cd68c1dc5b62e4094fe0f27eff
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2021-02-23 13:05:54 +0200

    Remove extra pipeline
    Remove the pipeline of mono camera used for tests.

--- depthai_ctrl.cpp
+++ depthai_ctrl.cpp
@@ -30,9 +30,6 @@ class DepthAICam
             auto colorCam = mPipeline.create<dai::node::ColorCamera>();
             auto colorCamVideoEnc = mPipeline.create<dai::node::VideoEncoder>();
             auto colorCamXLinkOut = mPipeline.create<dai::node::XLinkOut>();
-            auto monoCam1 = mPipeline.create<dai::node::MonoCamera>();
-            auto videoEnc = mPipeline.create<dai::node::VideoEncoder>();
-            auto videoEncXLinkOut = mPipeline.create<dai::node::XLinkOut>();
 
             colorCamXLinkOut->setStreamName("enc264Color");
             colorCam->setBoardSocket(dai::CameraBoardSocket::RGB);
@@ -43,17 +40,9 @@ class DepthAICam
             colorCam->video.link(colorCamVideoEnc->input);
             colorCamVideoEnc->bitstream.link(colorCamXLinkOut->input);
 
-            videoEncXLinkOut->setStreamName("enc264Left");
-            monoCam1->setBoardSocket(dai::CameraBoardSocket::LEFT);
-
-            videoEnc->setDefaultProfilePreset(1280, 720, 25, dai::VideoEncoderProperties::Profile::H264_MAIN);
-            monoCam1->out.link(videoEnc->input);
-            videoEnc->bitstream.link(videoEncXLinkOut->input);
-
             try {
                 device = new dai::Device(mPipeline, false);
                 encColorOutput = device->getOutputQueue("enc264Color", 30, true);
-                encLeftOutput = device->getOutputQueue("enc264Left", 30, true);
             } catch (const std::runtime_error& err) {
                 std::cout << "DepthAI runtime error: " << err.what() << std::endl;
                 mIsDeviceAvailable = false;
@@ -83,13 +72,11 @@ class DepthAICam
             return mIsDeviceAvailable;
         }
 
-        std::shared_ptr<dai::DataOutputQueue> encLeftOutput;
-        std::shared_ptr<dai::DataOutputQueue> encColorOutput;
-
     private:
         dai::Device *device;
         dai::Pipeline mPipeline;
         dai::CameraControl mCameraCtrl;
+        std::shared_ptr<dai::DataOutputQueue> encColorOutput;
         bool mIsDeviceAvailable = true;
 };
 
@@ -98,11 +85,8 @@ class DepthAIGst
 {
     public:
         DepthAIGst(int argc, char *argv[]) : depthAICam(nullptr), mLoop(nullptr), mPipeline(nullptr), mAppsrc(nullptr),
-                                            mH264parse(nullptr), mH264pay(nullptr),
-                                            mUdpSink(nullptr), mBusWatchId(0), mBus(nullptr), mNeedDataSignalId(0),
-                                            mPipeline2(nullptr), mAppsrc2(nullptr), mH264parse2(nullptr), mH264pay2(nullptr),
-                                            mUdpSink2(nullptr), mBusWatchId2(0), mBus2(nullptr), mNeedDataSignalId2(0),
-                                            mEnoughDataSignalId2(0), mLoopThread(nullptr)
+                                            mH264parse(nullptr), mH264pay(nullptr), mUdpSink(nullptr), mBusWatchId(0),
+                                            mBus(nullptr), mNeedDataSignalId(0), mLoopThread(nullptr)
         {
             gst_init(&argc, &argv);
             mLoop = g_main_loop_new(NULL, false);
@@ -134,9 +118,6 @@ class DepthAIGst
             if (mBusWatchId != 0) {
                 g_source_remove(mBusWatchId);
             }
-            if (mBusWatchId2 != 0) {
-                g_source_remove(mBusWatchId2);
-            }
         }
 
         void CreatePipeLine(void) {
@@ -166,36 +147,7 @@ class DepthAIGst
             mBus = nullptr;
             
             gst_bin_add_many(GST_BIN(mPipeline), mAppsrc, mH264parse, mH264pay, mUdpSink, NULL);
-            gst_element_link_many (mAppsrc, mH264parse, mH264pay, mUdpSink, NULL);
-
-            /* Second pipeline for encoded h264 video of the left camera. */
-            mPipeline2 = gst_pipeline_new("camUDPSink_pipeline2");
-            mAppsrc2 = gst_element_factory_make("appsrc", "source2");
-            g_object_set(G_OBJECT(mAppsrc2), "do-timestamp", true, NULL);
-            g_object_set(G_OBJECT(mAppsrc2), "is-live", true, NULL);
-            g_object_set(G_OBJECT(mAppsrc2), "block", true, NULL);
-            g_object_set(G_OBJECT(mAppsrc2), "stream-type", 0, NULL);
-            gst_util_set_object_arg(G_OBJECT(mAppsrc2), "format", "GST_FORMAT_TIME");
-            mH264parse2 = gst_element_factory_make("h264parse", "parser2");
-            mH264pay2 = gst_element_factory_make("rtph264pay", "payload2");
-            g_object_set(G_OBJECT(mH264pay2), "pt", 96, NULL);
-            mUdpSink2 = gst_element_factory_make("udpsink", "udp_sink2");
-            g_object_set(G_OBJECT(mUdpSink2), "host", "127.0.0.1", NULL);
-            g_object_set(G_OBJECT(mUdpSink2), "port", 5601, NULL);
-            g_object_set(G_OBJECT(mAppsrc2), "caps",
-                gst_caps_new_simple("video/x-h264",
-                    "width", G_TYPE_INT, 1280,
-                    "height", G_TYPE_INT, 720,
-                    "framerate", GST_TYPE_FRACTION, 25, 1,
-                    NULL), NULL);
-
-            mBus2 = gst_pipeline_get_bus(GST_PIPELINE(mPipeline2));
-            mBusWatchId2 = gst_bus_add_watch(mBus2, StreamEventCallBack2, this);
-            gst_object_unref(mBus2);
-            mBus2 = nullptr;
-            
-            gst_bin_add_many(GST_BIN(mPipeline2), mAppsrc2, mH264parse2, mH264pay2, mUdpSink2, NULL);
-            gst_element_link_many (mAppsrc2, mH264parse2, mH264pay2, mUdpSink2, NULL);
+            gst_element_link_many(mAppsrc, mH264parse, mH264pay, mUdpSink, NULL);
 
             mLoopThread = g_thread_new("GstThread", (GThreadFunc)DepthAIGst::PlayStream, this);
 
@@ -204,28 +156,11 @@ class DepthAIGst
             }
 
             mNeedDataSignalId = g_signal_connect(mAppsrc, "need-data", G_CALLBACK(NeedDataCallBack), this);
-            mNeedDataSignalId2 = g_signal_connect(mAppsrc2, "need-data", G_CALLBACK(NeedDataCallBack2), this);
-            mEnoughDataSignalId2 = g_signal_connect(mAppsrc2, "enough-data", G_CALLBACK(EnoughDataCallBack2), this);
         }
 
         void StopStream(void)
         {
-            GstFlowReturn ret, ret2;
-            if (mEnoughDataSignalId2 != 0) {
-                g_signal_handler_disconnect(mAppsrc2, mEnoughDataSignalId2);
-            }
-            if (mNeedDataSignalId2 != 0) {
-                g_signal_handler_disconnect(mAppsrc2, mNeedDataSignalId2);
-            }
-            if (mAppsrc2 != nullptr) {
-                g_signal_emit_by_name(mAppsrc2, "end-of-stream", &ret2);
-                if (ret2 != GST_FLOW_OK) {
-                    g_printerr("Error: Emit end-of-stream failed\n");
-                }
-            }
-            if (mPipeline2 != nullptr) {
-                gst_element_set_state(mPipeline2, GST_STATE_NULL);
-            }
+            GstFlowReturn ret;
 
             if (mNeedDataSignalId != 0) {
                 g_signal_handler_disconnect(mAppsrc, mNeedDataSignalId);
@@ -257,7 +192,6 @@ class DepthAIGst
             if (depthAICam != nullptr) {
                 depthAICam->StartStreaming();
             }
-            gst_element_set_state(depthAIGst->mPipeline2, GST_STATE_PLAYING);
             gst_element_set_state(depthAIGst->mPipeline, GST_STATE_PLAYING);
             g_main_loop_run(depthAIGst->mLoop);
             g_thread_exit(0);
@@ -266,73 +200,6 @@ class DepthAIGst
         }
 
         static gboolean StreamEventCallBack(GstBus *bus, GstMessage *message, gpointer data)
-        {
-            (void)bus;
-            g_print("%s: Got %s message\n", __FUNCTION__, GST_MESSAGE_TYPE_NAME(message));
- 
-            DepthAIGst *depthAIGst = (DepthAIGst *)data;
-
-            switch (GST_MESSAGE_TYPE (message)) {
-            case GST_MESSAGE_EOS:
-                g_print("End of stream\n");
-                g_main_loop_quit(depthAIGst->mLoop);
-                break;
-
-            case GST_MESSAGE_WARNING:
-                gchar  *warnDebug;
-                GError *warning;
-
-                gst_message_parse_warning(message, &warning, &warnDebug);
-                g_free(warnDebug);
-
-                g_printerr("Warning: %s\n", warning->message);
-                g_error_free(warning);
-                break;
-
-            case GST_MESSAGE_ERROR:
-                gchar  *errDebug;
-                GError *error;
-
-                gst_message_parse_error(message, &error, &errDebug);
-                g_free(errDebug);
-
-                g_printerr("Error: %s\n", error->message);
-                g_error_free(error);
-
-                g_main_loop_quit(depthAIGst->mLoop);
-                break;
-
-            default:
-                break;
-
-            }
-
-            return true;
-        }
-
-        static void NeedDataCallBack(GstElement *appsrc, guint unused_size, gpointer user_data)
-        {
-            (void)unused_size;
-            GstFlowReturn ret;
-            GstBuffer *buffer;
-            DepthAIGst *depthAIGst = (DepthAIGst *)user_data;
-            DepthAICam *depthAICam = depthAIGst->depthAICam;
-            
-            auto frame = depthAICam->encColorOutput->get<dai::ImgFrame>();
-
-            guint size = frame->getData().size();
-            buffer = gst_buffer_new_allocate(NULL, size, NULL);
-            gst_buffer_fill(buffer, 0, (gconstpointer)(frame->getData().data()), size);
-
-            g_signal_emit_by_name(appsrc, "push-buffer", buffer, &ret);
-            gst_buffer_unref(buffer);
-            if (ret != GST_FLOW_OK) {
-                /* something wrong, stop pushing */
-                g_main_loop_quit(depthAIGst->mLoop);
-            }
-        }
-
-        static gboolean StreamEventCallBack2(GstBus *bus, GstMessage *message, gpointer data)
         {
             (void)bus;
             g_print("%s: Got %s message\n", __FUNCTION__, GST_MESSAGE_TYPE_NAME(message));
@@ -387,15 +254,13 @@ class DepthAIGst
             return true;
         }
 
-        static void NeedDataCallBack2(GstElement *appsrc, guint unused_size, gpointer user_data)
+        static void NeedDataCallBack(GstElement *appsrc, guint unused_size, gpointer user_data)
         {
             (void)unused_size;
             GstFlowReturn ret;
             GstBuffer *buffer;
             DepthAIGst *depthAIGst = (DepthAIGst *)user_data;
-            DepthAICam *depthAICam = depthAIGst->depthAICam;
-            
-            auto frame = depthAICam->encLeftOutput->get<dai::ImgFrame>();
+            auto frame = depthAIGst->depthAICam->GetFrame();
 
             guint size = frame->getData().size();
             buffer = gst_buffer_new_allocate(NULL, size, NULL);
@@ -409,14 +274,6 @@ class DepthAIGst
             }
         }
 
-        static void EnoughDataCallBack2(GstElement *appsrc, guint unused_size, gpointer user_data)
-        {
-            (void)appsrc;
-            (void)unused_size;
-            (void)user_data;
-            g_print("%s\n", __FUNCTION__);
-        }
-
     private:
         GMainLoop *mLoop;
         GstElement *mPipeline;
@@ -427,15 +284,6 @@ class DepthAIGst
         guint mBusWatchId;
         GstBus *mBus;
         guint mNeedDataSignalId;
-        GstElement *mPipeline2;
-        GstElement *mAppsrc2;
-        GstElement *mH264parse2;
-        GstElement *mH264pay2;
-        GstElement *mUdpSink2;
-        guint mBusWatchId2;
-        GstBus *mBus2;
-        guint mNeedDataSignalId2;
-        guint mEnoughDataSignalId2;
         GThread *mLoopThread;
 };
 
