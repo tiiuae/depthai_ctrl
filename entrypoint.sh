17d2be0c088be477fe4c320ce37149239b648cfd
Mehmet Killioglu <mehmet.killioglu@unikie.com>
2022-01-26 10:42:34 +0200

    Use galactic as default build env, remove foxy from deb task

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /opt/ros/foxy/setup.bash
+source /opt/ros/galactic/setup.bash
 ros2 run depthai_ctrl camera_node --ros-args --remap __ns:=/$DRONE_DEVICE_ID -p address:=$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID
