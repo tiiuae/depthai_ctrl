85b7134a8bc57892ab1a7a81e43bf975bedc85b7
Jari Nippula <jarix@ssrc.tii.ae>
2022-04-05 19:33:07 +0300

    Fix containerization for fog_system

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-source /opt/ros/galactic/setup.bash
-ros2 run depthai_ctrl camera_node --ros-args --remap __ns:=/$DRONE_DEVICE_ID -p address:=$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID
+if [ "$1" = "gstreamer" ]; then
+    exec ros-with-env ros2 run depthai_ctrl gstreamer_node --ros-args --remap __ns:=/$DRONE_DEVICE_ID -p address:=$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID
+else
+    exec ros-with-env ros2 run depthai_ctrl camera_node --ros-args --remap __ns:=/$DRONE_DEVICE_ID -p address:=$RTSP_SERVER_ADDRESS/$DRONE_DEVICE_ID
+fi
