d62be631ff2ceb52459146a1783c6ac0bd12456b
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2022-11-10 14:10:26 +0200

    Keep exit on error as default
    Just wrap the command which we expect to give error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 _term() {
     # FILL UP PROCESS SEARCH PATTERN HERE TO FIND PROPER PROCESS FOR SIGINT:
@@ -36,11 +36,14 @@ fi
 child=$!
 
 echo "Waiting for pid $child"
-# Calling "wait" will then wait for the job with the specified by $child to finish, or for any signals to be fired.
-# Due to "or for any signals to be fired", "wait" will also handle SIGTERM and it will shutdown before
-# the node ends gracefully.
-# The solution is to add a second "wait" call and remove the trap between the two calls.
+# * Calling "wait" will then wait for the job with the specified by $child to finish, or for any signals to be fired.
+#   Due to "or for any signals to be fired", "wait" will also handle SIGTERM and it will shutdown before
+#   the node ends gracefully.
+#   The solution is to add a second "wait" call and remove the trap between the two calls.
+# * Do not use -e flag in the first wait call because wait will exit with error after catching SIGTERM.
+set +e
 wait $child
+set -e
 trap - TERM
 wait $child
 RESULT=$?
